f72afefd596069c59f76e5e8c5bf86d237148426
davidbkut <davidbk1974@gmail.com>
2019-08-16 19:14:05 +0000

    Images Blur #2

---
+++ image2.rb
@@ -0,0 +1,57 @@
+class Image
+
+  def initialize(picture)
+    @pic = picture
+  end
+
+  def output_image
+    @pic.each do |item|
+      puts item.join
+    end
+
+  end
+
+  def blur
+    locations = []
+    @pic.each_with_index do |row,rowindex|
+      #print "row= ",row,"   rowindex=",rowindex,"\n"
+      row.each_with_index do |column,columnindex|
+        #print "column= ",column,"   columnindex=",columnindex,"\n"
+        if column == 1
+          locations << [rowindex, columnindex]
+        end
+      end
+    end
+#print locations, "\n"
+
+  rmaxindex = @pic.length-1
+  cmaxindex = @pic[0].length-1
+    locations.each do |r, c|
+      if r > 0
+         @pic [r-1] [c] = 1    #change value above
+      end
+      if r < rmaxindex
+          @pic [r+1] [c] = 1    #change value below
+      end
+      if c > 0
+          @pic [r] [c-1] = 1    #change value to left
+      end
+      if c < cmaxindex
+          @pic [r] [c+1] = 1    #change value to right
+          #print "r= ",r,"  c=",c," \n"
+      end
+    end
+  end
+
+end
+
+
+image = Image.new([
+  [0, 0, 0, 0],
+  [0, 1, 0, 0],
+  [0, 0, 0, 1],
+  [0, 0, 0, 0]
+])
+
+image.blur
+image.output_image
